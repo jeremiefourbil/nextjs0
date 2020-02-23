04e05882bfbe2f005a5693e7b37699820427fb63
Jérémie FOURBIL <jeremie.fourbil@datadoghq.com>
2020-02-23 14:26:25 +0100

    test

--- api/index_test.go
+++ api/index_test.go
@@ -3,8 +3,8 @@ package api
 import "testing"
 
 func TestSum(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
+	total := Sum(4, 4)
+	if total != 8 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
